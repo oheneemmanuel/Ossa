e82c58bcb0afff013013e0ce7f687136e5975761
oheneemmanuel <ohenekwakye24@gmail.com>
2026-08-15 23:10:19 +0000

    fixed some errors

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,12 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 
-console.log("Cloudinary config check:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret_length: process.env.CLOUDINARY_API_SECRET?.length,
-  api_secret_first3: process.env.CLOUDINARY_API_SECRET?.slice(0, 3),
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
